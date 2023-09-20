16f8c3284b7c6f13cdc207fe427432a32d659101
KomalR2003 <komalribadiya0106@gmail.com>
2023-09-20 15:15:07 +0530

    first commit

---
+++ src/Component/NotFound.js
@@ -0,0 +1,8 @@
+function NotFound(){
+    return(
+        <>
+        <img src="https://images.ctfassets.net/hrltx12pl8hq/3Z1N8LpxtXNQhBD5EnIg8X/975e2497dc598bb64fde390592ae1133/spring-images-min.jpg" alt="not found"/>
+        </>
+    );
+}
+export default NotFound;
